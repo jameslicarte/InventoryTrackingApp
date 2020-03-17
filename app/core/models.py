ee23e0c31148b2a1e85e7116a0f2be19979828f2
James <james.licarte59@gmail.com>
2020-03-17 22:46:17 +0800

    added product model, test unit for product, added flake8

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """Product model"""
+    sku = models.CharField(max_length=15)
+    description = models.TextField()
+    date_added = models.DateTimeField()
+    stock = models.IntegerField()
+
+
