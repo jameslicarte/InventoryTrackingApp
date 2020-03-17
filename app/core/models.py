c8eb6b9b5b846ae89841ddb63cfd700c4867e586
James <james.licarte59@gmail.com>
2020-03-17 22:52:50 +0800

    minor changes to meet flake8 standards

--- app/core/models.py
+++ app/core/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
+
 class Product(models.Model):
     """Product model"""
     sku = models.CharField(max_length=15)
     description = models.TextField()
     date_added = models.DateTimeField()
     stock = models.IntegerField()
-
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,22 +1,20 @@
 from django.test import TestCase
 from core.models import Product
 
+
 class ModelTest(TestCase):
     def test_create_new_product_successful(self):
         """Test create new product is succesful"""
         sku = 'LEV-SHI-RED-SMA'
         product_id = 4
         product_obj = Product(
-            sku = sku,
-            id = product_id
+            sku=sku,
+            id=product_id
         )
 
         self.assertEqual(product_obj.sku, sku)
-        self.assertEqual(product_obj.id,product_id)
-
+        self.assertEqual(product_obj.id, product_id)
     # def test_product_quantity_recieved_added_to_inventory_successful(self):
     #     """Test product quantity received added to inventory successful"""
-
-
     # def test_product_deduct_in_inventory_successful(self):
     #     """Test product shiped quantity deducted in inventory successful"""
