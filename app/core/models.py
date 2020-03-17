771736d2e51d4eb4d22a55705bf60f8563f537c3
James <james.licarte59@gmail.com>
2020-03-18 00:04:10 +0800

    added identifiers for sku in product  model

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-
 class Product(models.Model):
-    """Product model"""
-    sku = models.CharField(max_length=15)
+    brand = models.CharField(max_length=255)
+    brand_id = models.CharField(max_length=3)
+    prodType = models.CharField(max_length=255)
+    prodType_id = models.CharField(max_length=3)
+    color = models.CharField(max_length=255)
+    color_id = models.CharField(max_length=3)
+    size = models.CharField(max_length=255)
+    size_id = models.CharField(max_length=3)
     description = models.TextField()
     date_added = models.DateTimeField()
     stock = models.IntegerField()
