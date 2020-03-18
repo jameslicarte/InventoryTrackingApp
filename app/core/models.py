0ef74ea8e60863a7a7b256efe5004d96cff2008e
James <james.licarte59@gmail.com>
2020-03-18 13:15:23 +0800

    flake8 standards

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.utils.timezone import datetime
+
+
 class Product(models.Model):
-    brand = models.CharField(max_length=255,default=None)
-    brand_id = models.CharField(max_length=3,default=None)
-    prodType = models.CharField(max_length=255,default=None)
-    prodType_id = models.CharField(max_length=3,default=None)
-    color = models.CharField(max_length=255,default=None)
-    color_id = models.CharField(max_length=3,default=None)
-    size = models.CharField(max_length=255,default=None)
-    size_id = models.CharField(max_length=3,default=None)
+    brand = models.CharField(max_length=255, default=None)
+    brand_id = models.CharField(max_length=3, default=None)
+    prodType = models.CharField(max_length=255, default=None)
+    prodType_id = models.CharField(max_length=3, default=None)
+    color = models.CharField(max_length=255, default=None)
+    color_id = models.CharField(max_length=3, default=None)
+    size = models.CharField(max_length=255, default=None)
+    size_id = models.CharField(max_length=3, default=None)
     description = models.TextField(default=None)
     date_added = models.DateTimeField(default=None)
     stock = models.IntegerField(default=None)
