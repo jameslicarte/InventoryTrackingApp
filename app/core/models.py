4f059d9782619a5afa52e3542d56673eef73bbb5
James <james.licarte59@gmail.com>
2020-03-18 13:09:26 +0800

    edited models

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-
+from django.utils.timezone import datetime
 class Product(models.Model):
-    brand = models.CharField(max_length=255)
-    brand_id = models.CharField(max_length=3)
-    prodType = models.CharField(max_length=255)
-    prodType_id = models.CharField(max_length=3)
-    color = models.CharField(max_length=255)
-    color_id = models.CharField(max_length=3)
-    size = models.CharField(max_length=255)
-    size_id = models.CharField(max_length=3)
-    description = models.TextField()
-    date_added = models.DateTimeField()
-    stock = models.IntegerField()
+    brand = models.CharField(max_length=255,default=None)
+    brand_id = models.CharField(max_length=3,default=None)
+    prodType = models.CharField(max_length=255,default=None)
+    prodType_id = models.CharField(max_length=3,default=None)
+    color = models.CharField(max_length=255,default=None)
+    color_id = models.CharField(max_length=3,default=None)
+    size = models.CharField(max_length=255,default=None)
+    size_id = models.CharField(max_length=3,default=None)
+    description = models.TextField(default=None)
+    date_added = models.DateTimeField(default=None)
+    stock = models.IntegerField(default=None)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,14 +5,14 @@ from core.models import Product
 class ModelTest(TestCase):
     def test_create_new_product_successful(self):
         """Test create new product is succesful"""
-        sku = 'LEV-SHI-RED-SMA'
+        brand = 'LEVIS'
         product_id = 4
         product_obj = Product(
-            sku=sku,
+            brand=brand,
             id=product_id
         )
 
-        self.assertEqual(product_obj.sku, sku)
+        self.assertEqual(product_obj.brand, brand)
         self.assertEqual(product_obj.id, product_id)
     # def test_product_quantity_recieved_added_to_inventory_successful(self):
     #     """Test product quantity received added to inventory successful"""
