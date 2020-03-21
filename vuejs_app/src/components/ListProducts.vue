babdb5eafc21a5800c8fe1f8865a37dcbfc6c97e
James <james.licarte59@gmail.com>
2020-03-21 14:11:41 +0800

    created a fresh install vuejs

---
+++ vuejs_app/src/components/ListProducts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1> Products </h1>  
+    <button v-on:click="getProducts">Get Products</button>
+  </div>  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Product",
+  props: ["product"],
+  data() {
+    return {
+      products : [],
+    }
+  },
+  methods: {
+    getProducts() {
+      axios.get("http://127.0.0.1:8000/api/products/")
+      .then(res => (this.videos = res.data))
+      .catch(err => console.log(err))
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
