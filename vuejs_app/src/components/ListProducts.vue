1ebe7add4e9b0fa99db977458570baaa6d8ca5d7
James <james.licarte59@gmail.com>
2020-03-25 23:42:18 +0800

    added ship product function with verification if stock is less than 0, changed some function names for receive product) "

--- vuejs_app/src/components/ListProducts.vue
+++ vuejs_app/src/components/ListProducts.vue
@@ -18,8 +18,8 @@
           <td>{{product.sku}}</td>
           <td>{{product.stock}}</td>
           <td>
-            <b-button variant="primary" v-on:click="promptAmount(); putProduct(product);">Receive</b-button>
-            <b-button variant="primary" style="margin-left: 10px;">Send</b-button>
+            <b-button variant="primary" v-on:click="promptAmount_receive(); putProduct_receive(product);">Receive</b-button>
+            <b-button variant="primary" v-on:click="promptAmount_send(); putProduct_ship(product);" style="margin-left: 10px;" >Send</b-button>
           </td>
         </tr>
 
@@ -47,7 +47,7 @@ export default {
         {id:'7', field: 'Stock'},
         {id:'8', field: 'Action'},
       ],
-      putProduct_data: [],
+      events_data: [],
     }
   },
   methods: {
@@ -56,26 +56,56 @@ export default {
       .then(res => (this.products = res.data))
       .catch(err => console.log(err));
     },
-    putProduct(product) {
+    putProduct_receive(product) {
+      var total_amount = product.stock + this.events_data.amount_receive
       axios.patch("http://127.0.0.1:8000/api/products/"+product.id+"/", {
-          stock: this.putProduct_data.amount,
+          stock: total_amount,
         },{
           headers: {
             'Content-Type': 'application/json'
           }
         })
         .then(function (response) {
-          console.log(response, this.putProduct_data.amount);
+          console.log(response, this.events_data.amount_receive);
         })
         .catch(function (error) {
           console.log(error);
         });
-      
     },
-    promptAmount() {
+    putProduct_ship(product) {
+      // Check first if product.stock > amount to send
+      if(product.stock < this.events_data.amount_ship){
+        console.log("ERROR! Stock cannot be less than 0");
+      }
+      else{
+        var total_amount = product.stock - this.events_data.amount_ship;
+        axios.patch("http://127.0.0.1:8000/api/products/"+product.id+"/", {
+            stock: total_amount,
+          },{
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(function (response) {
+            console.log(response, this.events_data.amount_ship);
+          })
+          .catch(function (error) {
+            console.log(error);
+          }); 
+      }
+
+    },
+    promptAmount_receive() {
       var amount = parseInt(prompt("Please enter amount to receive", 0));
       if (amount != null) {
-        this.putProduct_data.amount = amount;
+        this.events_data.amount_receive = amount;
+        console.log(amount);
+      }
+    },
+    promptAmount_send() {
+      var amount = parseInt(prompt("Please enter amount to ship", 0));
+      if (amount != null) {
+        this.events_data.amount_ship = amount;
         console.log(amount);
       }
     },
