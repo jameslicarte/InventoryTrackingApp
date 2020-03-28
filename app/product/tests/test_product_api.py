7d549c11841dfd40850c44c9f9e350876a418df1
James <james.licarte59@gmail.com>
2020-03-28 12:13:42 +0800

    added a test named test_api_product_create

--- app/product/tests/test_product_api.py
+++ app/product/tests/test_product_api.py
@@ -5,4 +5,29 @@ from core.models import Product, ProductType
 from rest_framework.test import APIClient
 from rest_framework import status
 
+CREATE_PRODUCT_URL = ('http://127.0.0.1:8000/api/products/')
+
+class PublicProductApiTest(TestCase):
+
+    def test_api_product_create(self):
+        """Test the products API""" 
+        prodType = 'T-Shirt'
+        product_name = 'Levis'
+        field1 = 'White'
+        field2 = 'Small'
+        field3 = 'Vneck'
+        payload = {
+          'prodType': prodType,
+          'product_name': product_name,
+          'field1': field1,
+          'field2': field2,
+          'field3': field3
+        }
+        
+        res = self.client.post(CREATE_PRODUCT_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+
+
+
 
